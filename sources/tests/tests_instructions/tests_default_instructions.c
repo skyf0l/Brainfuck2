2ee249660ff0c8f955253f7479dbe6f33abfdaa7
Skyf0l <tom.rorato@epitech.eu>
2020-05-21 01:53:10 +0200

    Add unit_tests for move_ptr right and left

--- sources/tests/tests_instructions/tests_default_instructions.c
+++ sources/tests/tests_instructions/tests_default_instructions.c
@@ -53,3 +53,31 @@ Test(add_and_sub, basic)
     memory_destroy(memory);
 }
 
+Test(move_ptr_right, basic)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+
+    cr_assert_not_null(memory);
+    cr_assert_eq(memory->tape_ptr, 0);
+    cr_assert_eq(move_ptr_right(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->tape_ptr, 1);
+    memory->tape_ptr = memory->tape_size - 2;
+    cr_assert_eq(move_ptr_right(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->tape_ptr, memory->tape_size - 1);
+    cr_assert_eq(move_ptr_right(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->tape_ptr, 0);
+    memory_destroy(memory);
+}
+
+Test(move_ptr_left, basic)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+
+    cr_assert_not_null(memory);
+    cr_assert_eq(memory->tape_ptr, 0);
+    cr_assert_eq(move_ptr_left(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->tape_ptr, memory->tape_size - 1);
+    cr_assert_eq(move_ptr_left(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->tape_ptr, memory->tape_size - 2);
+    memory_destroy(memory);
+}
