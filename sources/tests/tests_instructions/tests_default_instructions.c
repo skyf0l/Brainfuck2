199e13cd2f21980416ecebd9cdf010ec86ecf13a
Skyf0l <tom.rorato@epitech.eu>
2020-05-21 01:56:57 +0200

    Add test INSTRUCTIONS array

--- sources/tests/tests_instructions/tests_default_instructions.c
+++ sources/tests/tests_instructions/tests_default_instructions.c
@@ -6,6 +6,7 @@ Test(add, basic)
 {
     mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
 
+    cr_assert_eq(INSTRUCTIONS['+'], add);
     cr_assert_not_null(memory);
     cr_assert_eq(memory->tape[memory->tape_ptr], 0);
     cr_assert_eq(add(memory), EXIT_SUCCESS);
@@ -23,6 +24,7 @@ Test(sub, basic)
 {
     mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
 
+    cr_assert_eq(INSTRUCTIONS['-'], sub);
     cr_assert_not_null(memory);
     cr_assert_eq(memory->tape[memory->tape_ptr], 0);
     cr_assert_eq(sub(memory), EXIT_SUCCESS);
@@ -57,6 +59,7 @@ Test(move_ptr_right, basic)
 {
     mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
 
+    cr_assert_eq(INSTRUCTIONS['>'], move_ptr_right);
     cr_assert_not_null(memory);
     cr_assert_eq(memory->tape_ptr, 0);
     cr_assert_eq(move_ptr_right(memory), EXIT_SUCCESS);
@@ -73,11 +76,30 @@ Test(move_ptr_left, basic)
 {
     mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
 
+    cr_assert_eq(INSTRUCTIONS['<'], move_ptr_left);
     cr_assert_not_null(memory);
     cr_assert_eq(memory->tape_ptr, 0);
     cr_assert_eq(move_ptr_left(memory), EXIT_SUCCESS);
     cr_assert_eq(memory->tape_ptr, memory->tape_size - 1);
     cr_assert_eq(move_ptr_left(memory), EXIT_SUCCESS);
     cr_assert_eq(memory->tape_ptr, memory->tape_size - 2);
+    memory_destroy(memory);
+}
+
+Test(while_in, basic)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+
+    cr_assert_eq(INSTRUCTIONS['['], while_in);
+    cr_assert_not_null(memory);
+    memory_destroy(memory);
+}
+
+Test(while_out, basic)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+
+    cr_assert_eq(INSTRUCTIONS[']'], while_out);
+    cr_assert_not_null(memory);
     memory_destroy(memory);
 }
