8dabc2b2a18a39c20f4d8077ed2b19db4c9a8fd6
Skyf0l <tom.rorato@epitech.eu>
2020-05-22 12:50:40 +0200

    Add conditional_mid instruction

--- sources/src/interpretor/intructions/conditional_statement/conditional.c
+++ sources/src/interpretor/intructions/conditional_statement/conditional.c
@@ -25,6 +25,21 @@ int conditional_in(mem_t *mem)
 
 int conditional_mid(mem_t *mem)
 {
+    int conditional_layer = -1;
+
+    if (mem->tape[mem->tape_ptr])
+        return (EXIT_SUCCESS);
+    while (conditional_layer != 0 && mem->script.ops[mem->script.op_id]) {
+        ++(mem->script.op_id);
+        if (mem->script.ops[mem->script.op_id] == '!') 
+            ++conditional_layer;
+        else if (mem->script.ops[mem->script.op_id] == '?')
+            --(conditional_layer);
+        else if (!mem->script.ops[mem->script.op_id]) {
+            --(mem->script.op_id);
+            return (EXIT_SUCCESS);
+        }
+    }
     return (EXIT_SUCCESS);
 }
 
