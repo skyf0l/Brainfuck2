966cb74a30809a2b92fe53168764f0f8ab2b3097
Skyf0l <tom.rorato@epitech.eu>
2020-05-22 12:45:39 +0200

    Create unit_tests for confitional statement

--- sources/tests/tests_instructions/tests_conditional_instructions.c
+++ sources/tests/tests_instructions/tests_conditional_instructions.c
@@ -8,3 +8,142 @@ Test(instructions, conditional_statement)
     cr_assert_eq(INSTRUCTIONS[':'], conditional_mid);
     cr_assert_eq(INSTRUCTIONS['!'], conditional_out);
 }
+
+Test(conditional_in, basic_true)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"?:!";
+
+    cr_assert_not_null(memory);
+    add(memory);
+    cr_assert_eq(conditional_in(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 0);
+    memory_destroy(memory);
+}
+
+Test(conditional_in, basic_false)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"?:!";
+
+    cr_assert_not_null(memory);
+    cr_assert_eq(conditional_in(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 1);
+    memory_destroy(memory);
+}
+
+Test(conditional_in, eof_false_1)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"?";
+
+    cr_assert_not_null(memory);
+    cr_assert_eq(conditional_in(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 0);
+    memory_destroy(memory);
+}
+
+Test(conditional_in, eof_false_2)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"?+";
+
+    cr_assert_not_null(memory);
+    cr_assert_eq(conditional_in(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 1);
+    memory_destroy(memory);
+}
+
+Test(conditional_in, multi_false_1)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"??:!:!";
+
+    cr_assert_not_null(memory);
+    cr_assert_eq(conditional_in(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 4);
+    memory_destroy(memory);
+}
+
+Test(conditional_in, multi_false_2)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"???:!:?:!!?:!:!";
+
+    cr_assert_not_null(memory);
+    cr_assert_eq(conditional_in(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 13);
+    memory_destroy(memory);
+}
+
+Test(conditional_mid, basic)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"?:!";
+
+    cr_assert_not_null(memory);
+    memory->script.op_id = 1;
+    cr_assert_eq(conditional_mid(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 2);
+    memory_destroy(memory);
+}
+
+Test(conditional_mid, multi_1)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"?:?:!!";
+
+    cr_assert_not_null(memory);
+    memory->script.op_id = 1;
+    cr_assert_eq(conditional_mid(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 5);
+    memory_destroy(memory);
+}
+
+Test(conditional_mid, multi_2)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"!?:??:?:!!:?:!!!";
+
+    cr_assert_not_null(memory);
+    memory->script.op_id = 2;
+    cr_assert_eq(conditional_mid(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 15);
+    memory_destroy(memory);
+}
+
+Test(conditional_mid, eof_1)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"?:";
+
+    cr_assert_not_null(memory);
+    memory->script.op_id = 1;
+    cr_assert_eq(conditional_mid(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 1);
+    memory_destroy(memory);
+}
+
+Test(conditional_mid, eof_2)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"?:+";
+
+    cr_assert_not_null(memory);
+    memory->script.op_id = 1;
+    cr_assert_eq(conditional_mid(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 2);
+    memory_destroy(memory);
+}
+
+Test(conditional_out, basic)
+{
+    mem_t *memory = memory_create(DEFAULT_TAPE_SIZE);
+    memory->script.ops = (unsigned char *)"?:!";
+
+    cr_assert_not_null(memory);
+    memory->script.op_id = 2;
+    cr_assert_eq(conditional_out(memory), EXIT_SUCCESS);
+    cr_assert_eq(memory->script.op_id, 2);
+    memory_destroy(memory);
+}
